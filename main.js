7add065e70da45b0ba45c390245596ee1c45ce62
Agustin Torres <84466217+agustozzle@users.noreply.github.com>
2023-07-24 20:26:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,184 +1,80 @@
-let diasInput = document.getElementById("dias");
-  const dineroInicialInput = document.getElementById("dineroInicial");
-  let resultadoDiv = document.getElementById("resultado");
-  let opcionesDiv = document.getElementById("opciones");
+const dineroInicialInput = document.getElementById("dineroInicial");
+const calcularGastosButton = document.getElementById("calcularGastosButton");
+const resultadoDiv = document.getElementById("resultado");
 
-  function restar(a, b) {
-    return a - b;
+let gastosDiarios = [];
+
+function restar(a, b) {
+  return a - b;
+}
+
+function mostrarResultado(dineroInicial, gastosDiarios) {
+  const totalGastado = gastosDiarios.reduce((a, b) => a + b, 0);
+  const ahorro = restar(dineroInicial, totalGastado);
+  const dias = gastosDiarios.length;
+  resultadoDiv.textContent = `Su capital final en el día ${dias} es de $${dineroInicial - totalGastado}. Usted pudo ahorrar este mes: $${ahorro}`;
+
+  // Guardar los gastos en localStorage
+  localStorage.setItem("informeMes", JSON.stringify({ gastosDiarios }));
+}
+
+function calcularGastos() {
+  let dineroInicial = parseFloat(dineroInicialInput.value);
+  let dias = parseInt(document.getElementById("dias").value);
+
+  if (isNaN(dineroInicial) || isNaN(dias)) {
+    alert('Por favor, ingrese valores numéricos válidos.');
+    return;
   }
 
-  function calcularGastos() {
-    let dias;
-    let dineroInicial;
-    let saldoFinalDia;
-    let gastosDiarios = [];
+  let saldoFinalDia = dineroInicial;
+  gastosDiarios = []; // Limpiar gastosDiarios
 
-    dineroInicial = parseFloat(dineroInicialInput.value);
-    dias = parseInt(diasInput.value);
+  function ingresarGastoDia(i) {
+    if (i > dias) {
+      let totalGastado = gastosDiarios.reduce((a, b) => a + b, 0);
+      let ahorro = restar(dineroInicial, totalGastado);
 
-    if (isNaN(dineroInicial) || isNaN(dias)) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Por favor, ingrese valores numéricos válidos.'
-      });
+      if (saldoFinalDia < 0) {
+        alert(`Su capital es insuficiente, por favor recargue. Tiene que abonar: $${saldoFinalDia}`);
+      } else {
+        mostrarResultado(dineroInicial, gastosDiarios);
+      }
       return;
     }
 
-    saldoFinalDia = dineroInicial;
+    const dineroGastado = parseFloat(prompt(`Día ${i}: Ingrese cuánto gastó:`));
+    if (isNaN(dineroGastado)) {
+      alert('Por favor, ingrese un número válido.');
+      return;
+    }
 
-    function ingresarGastoDia(i) {
-      if (i >= dias) {
-        // Si ya se ingresaron todos los gastos, calculamos y mostramos el ahorro
-        let totalGastado = gastosDiarios.reduce((a, b) => a + b, 0);
-        let ahorro = restar(dineroInicial, totalGastado);
+    gastosDiarios.push(dineroGastado);
+    saldoFinalDia = restar(saldoFinalDia, dineroGastado);
 
-        if (saldoFinalDia < 0) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: `Su capital es insuficiente, por favor recargue. Tiene que abonar: $${saldoFinalDia}`
-          });
-        } else {
-          Swal.fire({
-            icon: 'info',
-            title: 'Información',
-            html: `Su capital final en el día ${i} es de $${saldoFinalDia}. Usted pudo ahorrar este mes: $${ahorro}`,
-            showConfirmButton: false,
-            timer: 10000
-          });
-        }
-        return;
-      }
+    if (saldoFinalDia < 0) {
+      alert(`Su capital es insuficiente, por favor recargue. Tiene que abonar: $${saldoFinalDia}`);
+      return;
+    }
 
-      Swal.fire({
-        icon: 'question',
-        title: `Día ${i + 1}`,
-        html: `
-          <label for="dineroGastado">Ingrese cuánto gastó en el día ${i + 1}:</label>
-          <input type="text" id="dineroGastado" class="swal2-input" />
-        `,
-        showCancelButton: true,
-        confirmButtonText: 'Aceptar',
-        cancelButtonText: 'Cancelar',
-        preConfirm: () => {
-          const dineroGastado = document.getElementById('dineroGastado').value;
-          if (isNaN(dineroGastado)) {
-            Swal.showValidationMessage('Por favor, ingrese un número válido.');
-          }
-          return parseFloat(dineroGastado);
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          const dineroGastado = result.value;
-          gastosDiarios.push(dineroGastado);
-          saldoFinalDia = restar(saldoFinalDia, dineroGastado);
+    ingresarGastoDia(i + 1);
+  }
 
-
-if (saldoFinalDia < 0) {
-  Swal.fire({
-    icon: 'error',
-    title: 'Error',
-    text: `Su capital es insuficiente, por favor recargue. Tiene que abonar: $${saldoFinalDia}`
-  });
-  return; // Detener la recursión si el saldo es negativo
+  ingresarGastoDia(1);
 }
 
+calcularGastosButton.addEventListener("click", calcularGastos);
 
-          
-          ingresarGastoDia(i + 1);
-        }
-      });
+dineroInicialInput.addEventListener("change", function () {
+  const savedData = localStorage.getItem("informeMes");
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    if (parsedData.gastosDiarios) {
+      gastosDiarios = parsedData.gastosDiarios;
     }
-
-    ingresarGastoDia(0);
   }
 
-  function mostrarOpciones() {
-    let opcionesHTML = `
-      <label for="menuOpciones">Seleccione una opción:</label>
-      <select id="menuOpciones">
-        <option value="buscar">Buscar un valor específico</option>
-        <option value="filtrar">Buscar valores mayores filtrados</option>
-      </select>
-      <button onclick="ejecutarOpcion()">Ejecutar Opción</button>
-    `;
+  mostrarResultado(parseFloat(dineroInicialInput.value), gastosDiarios);
+});
 
-    opcionesDiv.innerHTML = opcionesHTML;
-  }
-
-  function ejecutarOpcion() {
-    let opcionSeleccionada = document.getElementById("menuOpciones").value;
-    let resultadoOpcionDiv = document.createElement("div");
-
-    switch (opcionSeleccionada) {
-      case "buscar":
-        let gastoBuscado = parseFloat(prompt("Ingrese el valor de un gasto a buscar:"));
-
-        while (isNaN(gastoBuscado)) {
-          gastoBuscado = parseFloat(prompt("Por favor, ingrese un número válido para el valor de gasto a buscar:"));
-        }
-
-        let gastosDiarios = JSON.parse(localStorage.getItem("informeMes")).gastosDiarios;
-        let resultados = [];
-
-        for (let i = 0; i < gastosDiarios.length; i++) {
-          if (gastosDiarios[i] === gastoBuscado) {
-            resultados.push(`Día ${i + 1}: $${gastosDiarios[i]}`);
-          }
-        }
-
-        if (resultados.length > 0) {
-          resultadoOpcionDiv.innerHTML = `Los gastos de valor ${gastoBuscado} fueron encontrados en los siguientes días:<br>${resultados.join("<br>")}`;
-        } else {
-          resultadoOpcionDiv.innerHTML = `No se encontraron gastos de valor ${gastoBuscado}.`;
-        }
-        break;
-
-      case "filtrar":
-        let valorFiltro = parseFloat(prompt("Ingrese un valor para filtrar los gastos mayores a él:"));
-
-        while (isNaN(valorFiltro)) {
-          valorFiltro = parseFloat(prompt("Por favor, ingrese un número válido para el valor de filtro:"));
-        }
-
-        let gastosDiariosFiltrados = JSON.parse(localStorage.getItem("informeMes")).gastosDiarios.filter(gasto => gasto > valorFiltro);
-        let resultadosFiltrados = [];
-
-        for (let i = 0; i < gastosDiariosFiltrados.length; i++) {
-          resultadosFiltrados.push(`Día ${i + 1}: $${gastosDiariosFiltrados[i]}`);
-        }
-
-        if (resultadosFiltrados.length > 0) {
-          resultadoOpcionDiv.innerHTML = `Los gastos mayores a ${valorFiltro} son:<br>${resultadosFiltrados.join("<br>")}`;
-        } else {
-          resultadoOpcionDiv.innerHTML = `No se encontraron gastos mayores a ${valorFiltro}.`;
-        }
-        break;
-
-      default:
-        resultadoOpcionDiv.innerHTML = "Opción inválida";
-        break;
-    }
-
-    opcionesDiv.appendChild(resultadoOpcionDiv);
-  }
-
-  // const cardContent = document.getElementById("cardContent");
-
-  // function showNextRequirement(content) {
-  //   cardContent.innerHTML += content;
-  // }
-
-  dineroInicialInput.addEventListener("change", function () {
-    // Mostramos los siguientes requisitos solo cuando el usuario ingrese el dinero inicial
-    showNextRequirement('<br><label for="dias">Ingrese la cantidad de días para calcular su gasto (ingrese \'0\' para seleccionar por mes):</label>');
-    showNextRequirement('<br><input type="text" id="dias">');
-    showNextRequirement('<br><button onclick="calcularGastos()">Calcular Gastos</button>');
-    showNextRequirement('<br><div id="resultado"></div>');
-    showNextRequirement('<br><div id="menuOpciones"><button onclick="mostrarOpciones()">Mostrar Menú con Opciones</button><div id="opciones"></div></div>');
-
-    // Quitamos el evento de escucha después de mostrar los requisitos para evitar repetición
-    this.removeEventListener("change", arguments.callee);
-  });
 
